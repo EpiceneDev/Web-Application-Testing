e34467d719db8954518ca82a6c5f2dd38c833d44
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-05 14:07:49 -0700

    state set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display.js';
 import Dashboard from './components/Dashboard.js';
 
 function App() {
+  const [strike, setStrike] = useState(0);
+  const [ball, setBall] = useState(0);
+  const [foul, setFoul] = useState(0);
+
+  
+
   return (
     <div className="App">
       <Display  />
